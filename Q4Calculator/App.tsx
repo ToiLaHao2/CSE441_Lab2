1db28278d980ae0354df68b873a76969c6c11119
ToiLaHao2 <131520813+ToiLaHao2@users.noreply.github.com>
2023-12-03 23:26:41 +0700

    up

---
+++ Q4Calculator/App.tsx
@@ -0,0 +1,30 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import Button from 'react-native-paper';
+
+
+function App(): JSX.Element {
+  return (
+    <View>
+        <Button/>
+    </View>
+  );
+}
+
+export default App;
---
+++ Q4Calculator/style.js
@@ -0,0 +1,10 @@
+import {StyleSheet} from 'react-native';
+
+export default styles = StyleSheet.create({
+  numButton: {
+    borderColor: 'gray',
+    borderRadius: 10,
+    borderWidth: 1,
+    backgroundColor: 'white',
+  },
+});
